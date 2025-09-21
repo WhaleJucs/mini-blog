cce00ebe9d57246fe2bb5af49970e8eb46570f93
João <57973684+WhaleJucs@users.noreply.github.com>
2025-09-21 04:20:52 -0300

    CSS do navbar e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.css'
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt</h3>
             <p>Mini Blog &copy; 2025</p>
         </footer>

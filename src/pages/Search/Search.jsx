dda0269ec9ec15066418c510ece34ad7f709fbc7
João <57973684+WhaleJucs@users.noreply.github.com>
2025-10-19 12:53:09 -0300

    Exibindo resultado da busca e adicionando css a página de busca

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,15 +1,33 @@
+import { Link } from "react-router-dom"
+import styles from "./Search.module.css"
+
 // hooks
 import { useFetchDocuments } from "../../hooks/useFetchDocments"
 import { useQuery } from "../../hooks/useQuery"
+
+// Components
+import PostDetail from "../../components/PostDetail"
 
 const Search = () => {
     const query = useQuery()
     const search = query.get('q')
 
+    const { documents: posts } = useFetchDocuments('posts', search)
+
     return (
-        <div>
-            <h2>Search</h2>
-            <p>{search}</p>
+        <div className={styles.search_container}>
+            <h2>Buscas</h2>
+            <div>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram encontrados resultados...</p>
+                        <Link to='/' className="btn btn-dark">
+                            Voltar
+                        </Link>
+                    </div>
+                )}
+                {posts && posts.map((post) => (<PostDetail key={post.id} post={post} />))}
+            </div>
         </div>
     )
 }
